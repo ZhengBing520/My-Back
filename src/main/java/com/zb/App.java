269a778bf23fb798430cd3ff9711c3b6ba9ef6ae
ZhengBing520 <505986526@qq.com>
2019-12-15 20:12:29 +0800

    测试 actions

--- src/main/java/com/zb/App.java
+++ src/main/java/com/zb/App.java
@@ -17,6 +17,7 @@ public class App implements CommandLineRunner {
     }
 
     @Override
-    public void run(String... args) throws Exception {
+    public void run(String... args) {
+        System.out.println("start run ...");
     }
 }
